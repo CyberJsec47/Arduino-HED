dcb95fe44f5e78fd39276366dbd55424fb615aed
Cyber-Waves <149782160+CyberJsec47@users.noreply.github.com>
2024-10-17 20:00:33 +0100

    Update Main.ino
    updated first station test script with wind, direction and rain sensors added and published as MQTT messages

--- Main.ino
+++ Main.ino
@@ -1,19 +1,25 @@
+#include <SparkFun_Weather_Meter_Kit_Arduino_Library.h>
 #include <SparkFunBME280.h>
 #include <Wire.h>
 #include <WiFi.h>
 #include <PubSubClient.h>
 
 // WiFi details
- const char* ssid = "//My Wifi";
- const char* password = "//My Password";
+ const char* ssid = "//MyWIFI";
+ const char* password = "//MyPassword";
 
 // MQTT broker settings
- const char* mqtt_server = "//My server IP";
+ const char* mqtt_server = "//PI address";
 
  WiFiClient espClient;
  PubSubClient client(espClient);
 
+int windDirectionPin = 35;
+int windSpeedPin = 14;
+int rainfallPin = 27;
+
 BME280 tempSensor;
+SFEWeatherMeterKit weatherMeterKit(windDirectionPin, windSpeedPin, rainfallPin);
 
 unsigned long lastMsg = 0;
 #define MSG_INTERVAL 10000
@@ -65,6 +71,7 @@ void setup() {
    Serial.println("MQTT server set.");
 
   Wire.begin();
+  weatherMeterKit.begin();
   
   if (tempSensor.begin() == false) {
     Serial.println("BME280 did not respond.");
@@ -77,22 +84,31 @@ void readAndPrintSensorData() {
   float humidity = tempSensor.readFloatHumidity();
   float pressure = tempSensor.readFloatPressure();
   float altitude = tempSensor.readFloatAltitudeFeet();
+  float wind_dir = weatherMeterKit.getWindDirection();
+  float wind_speed = weatherMeterKit.getWindSpeed();
+  float rain = weatherMeterKit.getTotalRainfall();
 
   // Print data to Serial Monitor
   Serial.printf("Temperature: %.2f C\n", temperature_C);
   Serial.printf("Humidity: %.2f\n", humidity);
   Serial.printf("Pressure: %.2f Pa\n", pressure);
   Serial.printf("Altitude: %.2f feet\n", altitude);
+  Serial.printf("Wind Direction (Degrees): %.2f\n", wind_dir);
+  Serial.printf("Wind Speed (Kph): %.2f\n", wind_speed);
+  Serial.printf("Rainfall (mm): %.2f\n", rain);
   Serial.printf("-------------------------------\n");
  
   
   // Prepare strings for MQTT messages
-  char tempStr[50], humiStr[50], pressStr[50], altStr[50];
+  char tempStr[50], humiStr[50], pressStr[50], altStr[50], dirStr[50], speedStr[50], rainStr[50];
   
-  sprintf(tempStr, "Temperature is %.2f degrees C", temperature_C);
+  sprintf(tempStr, "Temperature is %.2f .C", temperature_C);
   sprintf(humiStr, "Humidity is %.2f", humidity);
   sprintf(pressStr, "Pressure is %.2f", pressure);
   sprintf(altStr, "Altitude is %.2f", altitude);
+  sprintf(dirStr, "Wind direction is %.2f degrees", wind_dir);
+  sprintf(speedStr, "Wind speed is %.2f Kph", wind_speed);
+  sprintf(rainStr, "There is %.2f mm of rainfall", rain);
 
   // Publish only if connected to MQTT
   if (client.connected()) {
@@ -100,6 +116,9 @@ void readAndPrintSensorData() {
     client.publish("sensor/humidity", humiStr);
     client.publish("sensor/pressure", pressStr);
     client.publish("sensor/altitude", altStr);
+    client.publish("sensor/direction", dirStr);
+    client.publish("sensor/speed", speedStr);
+    client.publish("sensor/rain", rainStr);
   }
 }
 
